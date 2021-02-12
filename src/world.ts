8c5b459dacd4a74c43990467bebe8c715c8b67a0
Shoyo Ito <ito@supinf.co.jp>
2021-02-13 08:25:26 +0900

    eslint,prettier,husky,lint-stagedの導入

--- src/world.ts
+++ src/world.ts
@@ -1,18 +1,18 @@
 export default class World {
-    // クラスで使うプロパティ
-    message: string
-  
-    // コンストラクタ（初期化）
-    constructor(message: string) {
-      this.message = message
+  // クラスで使うプロパティ
+  message: string
+
+  // コンストラクタ（初期化）
+  constructor(message: string) {
+    this.message = message
+  }
+
+  // 外部から呼び出せるメソッドを定義
+  public sayHello(elem: HTMLElement | null): string {
+    if (elem) {
+      return (elem.innerText = this.message)
+    } else {
+      return 'ng'
     }
-  
-    // 外部から呼び出せるメソッドを定義
-    public sayHello(elem: HTMLElement | null) {
-      if (elem) {
-        return (elem.innerText = this.message)
-      } else {
-        return
-      }
-    }
+  }
 }
